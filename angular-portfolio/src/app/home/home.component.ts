41303f452829b8615e4d3558f5d732e94d21c236
jihadftouny <jihad.santos@hotmail.com>
2023-08-22 14:07:50 +0300

    fixed paddings + added thinkAI

--- angular-portfolio/src/app/home/home.component.ts
+++ angular-portfolio/src/app/home/home.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  downloadCV(): void {
+    const link = document.createElement('a');
+    link.href = '../../assets/downloads/Jihad_Ftouny_CV.pdf';
+    link.target = '_blank';
+    link.download = 'Jihad_Ftouny_CV.pdf';
+    link.click();
+  }
 }
