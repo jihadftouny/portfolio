af0d2e4d475e9c251e5e29c3d1b9df19a5b2935f
jihadftouny <jihad.santos@hotmail.com>
2023-08-22 17:27:07 +0300

    start sound over again

--- angular-portfolio/src/app/home/home.component.ts
+++ angular-portfolio/src/app/home/home.component.ts
@@ -1,79 +1,11 @@
-import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  isMuted: boolean = false;
-  backgroundMusic: HTMLAudioElement | null = null;
-
-  constructor(private renderer: Renderer2) {}
-
-  ngOnInit() {
-    // Get a reference to the audio element
-    this.backgroundMusic = document.getElementById(
-      'backgroundMusic'
-    ) as HTMLAudioElement;
-    // Set the volume to a lower level (0.5 for example)
-    if (this.backgroundMusic) {
-      this.backgroundMusic.volume = 0.1; // Adjust the volume level here
-      this.backgroundMusic.play();
-    }
-  }
-
-  @HostListener('mouseenter', ['$event'])
-  onMouseEnter(event: MouseEvent) {
-    const targetElement = event.target as HTMLElement;
-    this.playHoverSound();
-  }
-
-  @HostListener('mouseenter', ['$event'])
-  onMouseEnterAbout(event: MouseEvent) {
-    const targetElement = event.target as HTMLElement;
-    this.playHoverSoundAbout();
-  }
-
-  //having this empty function solves the issue when a sound is played when you hover outside the page then in again
-  @HostListener('mouseenter', ['$event'])
-  onMouseEnterAbout2(event: MouseEvent) {
-    const targetElement = event.target as HTMLElement;
-  }
-
-  playHoverSoundAbout() {
-    const audio = this.renderer.createElement('audio');
-    audio.src = '../assets/audio/hover-sound-about.mp3'; // Replace with the actual path
-    audio.autoplay = true;
-    audio.style.display = 'none';
-
-    this.renderer.appendChild(document.body, audio);
-
-    audio.onended = () => {
-      this.renderer.removeChild(document.body, audio);
-    };
-  }
-  playHoverSound() {
-    const audio = this.renderer.createElement('audio');
-    audio.src = '../assets/audio/hover-sound.mp3'; // Replace with the actual path
-    audio.autoplay = true;
-    audio.style.display = 'none';
-
-    this.renderer.appendChild(document.body, audio);
-
-    audio.onended = () => {
-      this.renderer.removeChild(document.body, audio);
-    };
-  }
-
-  toggleMute() {
-    // Toggle the muted state of the audio element
-    this.isMuted = !this.isMuted;
-    if (this.backgroundMusic) {
-      this.backgroundMusic.muted = this.isMuted;
-    }
-  }
-
+export class HomeComponent {
   downloadCV(): void {
     const link = document.createElement('a');
     link.href = '../../assets/downloads/Jihad_Ftouny_CV.pdf';
