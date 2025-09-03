64cf99607a70bbfff18b6cda3bbed363e2c7ec0c
jihadftouny <jihad.santos@hotmail.com>
2025-09-03 21:55:49 +0300

    now can access / and /blog

--- angular-portfolio/src/app/app-routing.module.ts
+++ angular-portfolio/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
 import { CaseStudiesComponent } from './case-studies/case-studies.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
   { path: 'case-studies', component: CaseStudiesComponent },
 ];
 
