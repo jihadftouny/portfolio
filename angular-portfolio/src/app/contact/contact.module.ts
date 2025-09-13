612a6a702659cbf85c5e6d37016bb03efed7cd88
jihadftouny <jihad.santos@hotmail.com>
2025-09-14 00:26:30 +0300

    fixed vercel errors

---
+++ angular-portfolio/src/app/contact/contact.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ContactComponent } from './contact.component';
+
+@NgModule({
+  declarations: [ContactComponent],
+  imports: [CommonModule, RouterModule.forChild([{ path: '', component: ContactComponent }])]
+})
+export class ContactModule {}
---
+++ angular-portfolio/src/polyfills.ts
@@ -0,0 +1,11 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ *
+ * The current setup is a minimal polyfills file to satisfy Angular build.
+ */
+
+/** Zone JS is required by default for Angular. */
+import 'zone.js';
+
+// If you need additional polyfills for older browsers, add them here.
